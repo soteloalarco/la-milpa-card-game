79e4c985f2a4132a11a71492f9780b1c1e657986
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-11-16 04:14:11 -0600

    planting seeds

--- la-milpa-frontend/src/pages/match/components/CropCardMilpa.jsx
+++ la-milpa-frontend/src/pages/match/components/CropCardMilpa.jsx
@@ -1,29 +1,44 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useRecoilValue} from "recoil";
-import {milpaP1CropsState, milpaP2CropsState} from "../../../features/todos/atoms";
+import { useRecoilState} from "recoil";
+import {milpaP1CropsState, milpaP2CropsState, detailsP1State, detailsP2State} from "../../../features/todos/atoms";
 
 export default function CropCardMilpa(props){
 
   const ACTIVE_CLASS="w-12 h-12 text-lg";
   const INACTIVE_CLASS="w-12 h-12 text-lg cursor-not-allowed";
 
-  const milpaP1CropsValue = useRecoilValue(milpaP1CropsState);
-  const milpaP2CropsValue = useRecoilValue(milpaP2CropsState);
+  const [milpaP1CropsValue, SetMilpaP1CropsState] = useRecoilState(milpaP1CropsState);
+  const [milpaP2CropsValue, SetMilpaP2CropsState] = useRecoilState(milpaP2CropsState);
+  const [detailsP1Value, setDetailsP1State] = useRecoilState(detailsP1State);
+  const [detailsP2Value, setDetailsP2State] = useRecoilState(detailsP2State);
 
   // eslint-disable-next-line no-unused-vars
-  const displayIconCrop = () => {
-    if (props.key/16 < 1){
-      return milpaP1CropsValue[props.indexMilpaCrop]
+  const handleOnClick = () => {
+    if(props.indexMilpaCrop/16<1){
+      // eslint-disable-next-line prefer-const
+      let newMilpa = milpaP1CropsValue.slice();
+      newMilpa[props.indexMilpaCrop]= {
+        key : detailsP1Value.key,
+        icon : detailsP1Value.icon,
+      };
+      SetMilpaP1CropsState(newMilpa);
+    } else{
+      // eslint-disable-next-line prefer-const
+      let newMilpa = milpaP2CropsValue.slice();
+      newMilpa[props.indexMilpaCrop]= {
+        key : detailsP2Value.key,
+        icon : detailsP2Value.icon,
+      }
+      SetMilpaP2CropsState(newMilpa)
     }
-    
-    return milpaP2CropsValue[props.indexMilpaCrop%16]
-    
+   
   }
 
   return (
-    <div className="parcela md:w-16 md:h-16"><button type="button" className={props.isActive ? ACTIVE_CLASS : INACTIVE_CLASS} disabled={!props.isActive}>{props.displayCrop.icon}</button></div>
+    <div className="parcela md:w-16 md:h-16"><button type="button" className={props.isActive ? ACTIVE_CLASS : INACTIVE_CLASS} onClick={handleOnClick} disabled={!props.isActive}>{props.displayCrop.icon}</button></div>
   )
 }
 
